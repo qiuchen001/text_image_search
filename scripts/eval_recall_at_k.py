a06cdf6b7a511442bfe5b8d86f018da33ced409e
zhangchenchen <zhangchenchen@51aes.com>
2025-06-04 18:39:16 +0800

    格式化

--- scripts/eval_recall_at_k.py
+++ scripts/eval_recall_at_k.py
@@ -26,12 +26,19 @@ def eval_text2image_timeofday_recall_at_k(label_jsonl_path, operator, k=5, query
     for r in results:
         img_path = r['path']
         img_id = os.path.splitext(os.path.basename(img_path))[0]
+        if id2timeofday.get(img_id) is None:
+            print(f"{img_id}的标签为None")
+            k -= 1
+            continue
+
         if id2timeofday.get(img_id) == query_time:
             hit += 1
+        else:
+            print(f"{img_id}的标签是{id2timeofday.get(img_id)}, 查询结果是{query_time}")
     recall = hit / k if k > 0 else 0
     print(f"用文本'{query_time}'检索，Recall@{k} = {recall:.4f}，命中{hit}/{k}")
 
 
 if __name__ == '__main__':
     label_jsonl_path = 'bdd100k_labels_timeofday.jsonl'
-    eval_text2image_timeofday_recall_at_k(label_jsonl_path, text_image_vector, k=5, query_time='白天')
+    eval_text2image_timeofday_recall_at_k(label_jsonl_path, text_image_vector, k=500, query_time='白天')
